77ea7dc41a79d9a2278a3265af6bccc6e2010f8e
Carson Meiling <carmc93@gmail.com>
2020-03-25 16:29:09 -0600

    add semantic, starting app.js and api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {restaurants: []}
+
+
+  componentDidMount(){
+    axios.get('https://api.yelp.com/v3/businesses/{id}')
+    .then(res => {
+      this.setState({restaurants: res.data})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  render() {
+    const { restaurants } = this.state
+    return(
+      <>
+      < restaurants />
+      </>
+    )
+  }
 }
+
+
 
 export default App;
